7a07eed1c91f17cca8f8f1fe8cbc3e8bd016dfdf
gbraccialli <gbraccialli@gmail.com>
2015-04-02 21:21:59 -0300

    add wget and add jar

--- scripts/create_function.sql
+++ scripts/create_function.sql
@@ -1,2 +1,5 @@
+--wget https://github.com/gbraccialli/GeohashHiveUDF/raw/master/target/GeohashHiveUDF-1.0-SNAPSHOT-jar-with-dependencies.jar
+add jar /jars/GeohashHiveUDF-1.0-SNAPSHOT-jar-with-dependencies.jar;
+
 CREATE TEMPORARY FUNCTION GeohashEncode as 'com.github.gbraccialli.GeohashHiveUDF.UDFGeohashEncode';
 CREATE TEMPORARY FUNCTION GeohashDecode as 'com.github.gbraccialli.GeohashHiveUDF.UDFGeohashDecode';
